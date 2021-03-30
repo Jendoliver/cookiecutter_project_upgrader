f6341e1161f308143d247a9fdd0da568d98e763a
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2021-03-30 13:46:50 +0200

    remove travis-related feature in initial.py

--- dev-util/initial.py
+++ dev-util/initial.py
@@ -1,17 +1,15 @@
 """
 Script to execute after creating the project.
-- Encrypts the PyPi password to the .travis.yml
 - Updates PyCharm configuration if detected
 -- Sets the test runner to pytest
 -- Excludes cache directories
 """
 
-import configparser
 import subprocess
+import xml.etree.ElementTree as ElementTree
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Tuple, cast, Dict, Optional
-import xml.etree.ElementTree as ElementTree
+from typing import cast, Dict
 
 try:
     import yaml
@@ -23,72 +21,6 @@ except ImportError as error:
     except ImportError as error:
         raise Exception(
             "Could not install pyyaml automatically successfully, please install it manually first") from error
-
-TRAVIS_YML_FILE = Path("../.travis.yml").expanduser()
-TRAVIS_YML_ENCRYPTED_PASSWORD_PLACEHOLDER_VALUE = "PLEASE_REPLACE_ME"
-
-
-def encrypt_pypi_password_for_travis_if_necessary():
-    travis_yml_content = yaml.safe_load(TRAVIS_YML_FILE.read_text(encoding="utf-8"))
-    deploy_job = travis_yml_content["jobs"]["include"][-1]
-    if deploy_job["stage"] == "deploy":
-        project_owner_pypi_username = deploy_job["deploy"]["user"]
-        project_owner_pypi_encrypted_password = deploy_job["deploy"]["password"]["secure"]
-        if project_owner_pypi_encrypted_password == TRAVIS_YML_ENCRYPTED_PASSWORD_PLACEHOLDER_VALUE:
-            _encrypt_pypi_password_for_travis(project_owner_pypi_username)
-        else:
-            print(f"Travis password has already been encrypted in {TRAVIS_YML_FILE.name}.")
-    else:
-        print("Travis is not deploying to PyPi, skipping check for PyPi credentials")
-
-
-def _encrypt_pypi_password_for_travis(project_owner_pypi_username: str):
-    pypi_username, pypi_password = _get_pypi_credentials()
-    if pypi_username != project_owner_pypi_username:
-        raise Exception(f"The PyPi password for Travis has not yet been configured but "
-                        f"the local pypi credentials' username ({pypi_username}) "
-                        f"does not match the pypi username stated "
-                        f"in {TRAVIS_YML_FILE.name} ({project_owner_pypi_username}). "
-                        f"Please check your ~/.pypirc file or your keyring properties for pypi.")
-    try:
-        result = subprocess.run(
-            ["travis", "encrypt", "--skip-version-check", "--skip-completion-check", "--no-interactive"],
-            input=bytes(pypi_password, encoding="utf-8"),
-            capture_output=True,
-        )
-    except FileNotFoundError as exception:
-        raise Exception("Command 'travis' needs to be available on path. "
-                        "Please install https://github.com/travis-ci/travis.rb#installation") from exception
-    result.check_returncode()
-    output = str(result.stdout, encoding="utf-8")
-    encrypted_password = output[1:-2]
-
-    travis_yml_text = TRAVIS_YML_FILE.read_text(encoding="utf-8")
-    travis_yml_text = travis_yml_text.replace(TRAVIS_YML_ENCRYPTED_PASSWORD_PLACEHOLDER_VALUE, encrypted_password)
-    TRAVIS_YML_FILE.write_text(travis_yml_text, encoding="utf-8")
-    print(f"Updated {TRAVIS_YML_FILE.name} with encrypted pypi password. Please commit it in Git.")
-
-
-def _get_pypi_credentials() -> Tuple[str, str]:
-    pypirc_file = Path("~/.pypirc").expanduser()
-    if pypirc_file.is_file():
-        config = configparser.ConfigParser()
-        config.read(pypirc_file)
-        pypi_username = config['pypi']['username']
-        pypi_password = config['pypi']['password']
-        print(f"Using pypi credentials found in {pypirc_file}")
-        return pypi_username, pypi_password
-    else:
-        # TODO check keyring if pypi credentials are there in case ~/.pypirc does not exist.
-        #  https://twine.readthedocs.io/en/latest/#id6
-        raise Exception("Please create the file ~/.pypirc with your pypi user credentials as documented at "
-                        "https://packaging.python.org/guides/distributing-packages-using-setuptools/#id78\n"
-                        "Support for the Keyring credentials as documented at "
-                        "https://twine.readthedocs.io/en/latest/#id6 is currently unsupported but contributing "
-                        "is wished.")
-
-
-encrypt_pypi_password_for_travis_if_necessary()
 
 
 def _get_project_name():
